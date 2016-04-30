fbd2d734e2560d75c6473e8153461c4d2ad520ab
Alex Dawson-Elli <dawsonelli@wisc.edu>
2016-04-30 17:25:19 -0500

    added style guide to project

---
+++ PythonCode/styleGuide.py
@@ -0,0 +1,20 @@
+"""
+Author: Alex Dawson-Elli
+
+styleGuide for Hemineglect project: 
+	-all functions (methods) are to be written in lower camelCase
+	-all classes start with a capital letter
+	-one class per file, name the file after the class
+	-all objects start with a lowercase letter, and are frequently
+	related to the class by name 
+	-each method starts with a docstring
+	-the first line of the docstring is shorthand for the input and 
+	output type of the function, connected by an arrow
+		-ex:   "int -> float " for a function that takes an
+		int and returns a float
+	-all indentations are tabs
+	-2 blank lines between functions
+	-line up equals signs where possible for similar sequential assignments
+
+"""
+
