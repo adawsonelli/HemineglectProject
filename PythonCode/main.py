b7eec2e94c37cbf3a695a5d7304968272fed1de0
Alex Dawson-Elli <dawsonelli@wisc.edu>
2016-05-03 23:55:18 -0500

    added a main.py to instantiate classes, and perform simulation and analysis

---
+++ PythonCode/main.py
@@ -0,0 +1,14 @@
+import Simulator as Sim
+import AnalysisTools as Analysis
+
+#instantiate simulator
+sim = Sim.Simulator()
+sim.exampleData()
+
+#read in
+anaTool = Analysis.AnalysisTools()
+anaTool.loadData('exampleData.txt')
+
+
+#display statistics
+anaTool.displayStatistics()
